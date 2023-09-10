28207fe749d6a13fad2bdd2b747806b6570fa6ed
ianbowman123 <144627599+ianbowman123@users.noreply.github.com>
2023-09-10 18:09:41 -0500

    wert
    sert

---
+++ Assets/Prefabs/IanPrefabs/PigjamTrackspawnII.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PigjamTrackspawnII : MonoBehaviour
+{
+    public Transform TrackSpawnPoint;
+    public GameObject TrackBlock;
+
+    void Start()
+    {
+        Instantiate(TrackBlock, TrackSpawnPoint.position);
+        Instantiate(TrackBlock, TrackSpawnPoint.position + Vector3.back * 10f);
+        Instantiate(TrackBlock, TrackSpawnPoint.position + Vector3.back * 20f);
+
+    }
+    void Update()
+    {
+        if(FindAnyObjectByType(Level_Block).transform.position.z < -10f)
+        {
+            Destroy(GameObject);
+            Instantiate(TrackBlock, TrackSpawnPoint.position);
+        }
+    }
+
+    
+}
