62e14426564f9fabd636547f56350efe5086fcb2
DEX1227 <144620085+DEX1227@users.noreply.github.com>
2023-09-10 21:12:08 -0500

    Update SongManager.cs

--- Assets/Game/Scripts/SongManager.cs
+++ Assets/Game/Scripts/SongManager.cs
@@ -10,7 +10,7 @@ using System;
 public class SongManager : MonoBehaviour
 {
     //the current position of the song (in seconds)
-     public float songPosition;
+    public float songPosition;
 
     //the current position of the song (in beats)
     public float songPosInBeats;
@@ -27,14 +27,33 @@ public class SongManager : MonoBehaviour
     public float bpm;
 
     //keep all the position-in-beats of notes in the song
-    public float[] notes;
+    public float[] notes = { 1f, 1.5f, 2, 2.5f, 3.5f, 4f, 4.5f, 9, 9.5f, 10, 10.5f, 11.5f, 12, 12.5f, 14.5f, 15, 16, 16.5f, 17, 17.5f, 18, 18.5f, 19.5f, 20, 20.5f, 25, 25.5f, 26, 26.5f, 27.5f, 28, 28.5f, 30, 30.5f, 31, 31.5f, 32 };
 
     //the index of the next note to be spawned
     public int nextIndex = 0;
 
-    public Action SpawnKeyNote;
+    public Action<float> SpawnKeyNote;
 
     public AudioSource audio;
+
+    public GameObject note;
+
+   /* float[] bassNotesBars1To8 = { 1f, 1.5f, 2, 2.5f, 3.5f, 4f, 4.5f, 9, 9.5f, 10, 10.5f, 11.5f, 12, 12.5f, 14.5f, 15, 16, 16.5f, 17, 17.5f, 18, 18.5f, 19.5f, 20, 20.5f, 25, 25.5f, 26, 26.5f, 27.5f, 28, 28.5f, 30, 30.5f, 31, 31.5f, 32 };
+
+    //add 32 to these values
+    float[] bassNotesBars9To16 = { 1f, 1.5f, 2, 2.5f, 3.5f, 4f, 4.5f, 9, 9.5f, 10, 10.5f, 11.5f, 12, 12.5f, 14.5f, 15, 16, 16.5f, 17, 17.5f, 18, 18.5f, 19.5f, 20, 20.5f, 25, 25.5f, 26, 26.5f, 27.5f, 28, 28.5f, 29, 29.5f, 30, 30.5f, 31, 31.5f, 32, 32.5f };
+
+    //add 64 to these values
+    float[] pianoNotesBars17To24 = { 1, 2.5f, 6.5f, 9, 10.5f, 17, 18.5f, 21, 21.5f, 22, 22.5f, 23, 24, 24.5f, 29.5f, 31 };
+
+    //add 96 to these values
+    float[] pianoNotesBars25To32 = { 1, 2.5f, 4.5f, 6, 9, 10.5f, 12.5f, 14, 17, 18, 19.5f, 20.5f, 22.5f, 23, 23.5f, 24, 25, 26, 27.5f, 28.5f, 30.5f, 31, 31.5f, 32 };
+
+    //add 128 to these values
+    float[] guitarNotesBars33To40 = { 1, 5.5F, 6, 6.5f, 7, 9, 10, 10.5f, 13.5f, 14, 14.5f, 17.5f, 18, 18.5f, 19, 19.5f, 20.5f, 21.5f, 22, 22.5f, 26, 26.5f, 28, 28.5f, 30, 30.5f, 32 };
+
+    //add 160 to these values
+    float[] guitarNotesBars41to48 = { 1, 7, 8, 8.5f, 9, 10, 10.5f, 11.5f, 12, 13, 13.5f, 14, 14.5f, 15, 15.5f, 16, 16.5f, 17, 18, 18.5f, 19, 20, 20.5f, 25, 26.6f, 28 };*/
 
     void Start()
     {
@@ -57,7 +76,9 @@ public class SongManager : MonoBehaviour
         //calculate the position in beats
         songPosInBeats = songPosition / secPerBeat;
         if (nextIndex < notes.Length && notes[nextIndex] < songPosInBeats + beatsShownInAdvance)
-            SpawnKeyNote.Invoke();
+            SpawnKeyNote.Invoke(nextIndex);
+
+        nextIndex++;
     }
 
 }
