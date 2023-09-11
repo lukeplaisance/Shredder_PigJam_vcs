a639cd3d3a4894fb345ab32dfdf01c1966c819b1
jtculbreth <jtculbreth@gmail.com>
2023-09-10 19:27:40 -0500

    Bass Notes Script

---
+++ Assets/Game/Scripts/BassNotes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BassNotes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] bassNotesBars1To8 = { 1f, 1.5f, 2, 2.5f, 3.5f, 4f, 4.5f, 9, 9.5f, 10, 10.5f, 11.5f, 12, 12.5f,14.5f,15,16,16.5f,17,17.5f,18,18.5f,19.5f,20,20.5f,25,25.5f,26,26.5f,27.5f,28,28.5f,30,30.5f,31,31.5f,32 };
+        float[] bassNotesBars9To16 = { 1f, 1.5f, 2, 2.5f, 3.5f, 4f, 4.5f, 9, 9.5f, 10, 10.5f, 11.5f, 12, 12.5f, 14.5f, 15, 16, 16.5f, 17, 17.5f, 18, 18.5f, 19.5f, 20, 20.5f, 25, 25.5f, 26, 26.5f, 27.5f, 28, 28.5f, 29, 29.5f, 30, 30.5f, 31, 31.5f,32,32.5f };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
